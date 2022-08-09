6c5e149e6603903000de40341c3458f05e1214be
Prost0Lime <s.shishkov2026@gmail.com>
2022-08-09 13:54:50 +0300

    initial commit

---
+++ laba2/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: adfgh
+ * Date: 01-03-2019
+ * Time: 13:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace laba2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(52, 24);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(52, 50);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.TextChanged += new System.EventHandler(this.TextBox2TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(174, 20);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "ввод";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(174, 64);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "вывод";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(31, 23);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "a";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 50);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(20, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "b";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 79);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(21, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "c";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(52, 79);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 8;
+			this.textBox3.TextChanged += new System.EventHandler(this.TextBox3TextChanged);
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(130, 115);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(10, 115);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 33);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "площадь боковой поверхности";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(10, 159);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "объём";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(130, 159);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(100, 23);
+			this.label7.TabIndex = 12;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(266, 21);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(84, 23);
+			this.button3.TabIndex = 13;
+			this.button3.Text = "Локальный";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(356, 193);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "Рассчёт данных призмы";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+	}
+}
---
+++ laba2/MainForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: adfgh
+ * Date: 01-03-2019
+ * Time: 13:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace laba2
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		Parallelepiped P=new Parallelepiped ();
+		void Button1Click(object sender, EventArgs e)
+		{
+			P.setA (Convert.ToDouble(textBox1.Text));
+			P.setB (Convert.ToDouble(textBox2.Text));	
+			P.setC (Convert.ToDouble(textBox3.Text));			
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			label4.Text=P.Ploschad().ToString();
+			label7.Text=P.Obem().ToString();
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			Parallelepiped P=new Parallelepiped ();	
+			P.setA (Convert.ToDouble(textBox1.Text));
+			P.setB (Convert.ToDouble(textBox2.Text));
+			P.setC (Convert.ToDouble(textBox3.Text));
+			label4.Text=P.Ploschad().ToString();
+			label7.Text=P.Obem().ToString();
+		}
+		void TextBox2TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox3TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		class Parallelepiped {
+			double a, b, c;
+			public void setA (double X) { a=X ;}
+			public void setB (double X) { b=X ;}
+			public void setC (double X) { c=X ;}
+			public double geta () {return a;}
+			public double getb () {return b;}
+			public double getc() {return c;}
+			public double Ploschad () {return 2*getc()*(geta()+getb()) ;}
+			public double Obem () {return geta()*getb()*getc() ;}
+		}
+		
+	}
+}
